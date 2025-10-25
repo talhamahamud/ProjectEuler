d85c52773a621afcb6069adcae8dfc3dbff0ec4a
Talha Mahamud <95077791+talhamahamud@users.noreply.github.com>
2025-10-25 16:26:00 +0600

    Add new folder via GUI

---
+++ C++/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k = 0;
+
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            k = k + i;
+        }
+    }
+
+    cout << k << endl;  // The answer is 233168
+    return 0;
+}
